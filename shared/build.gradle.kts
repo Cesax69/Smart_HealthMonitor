77f53c1e6071af62152164aa0cec0634eba4d4d2
Cesax69 <cesar20437@cbtis75.edu.mx>
2026-06-09 16:40:00 -0600

    feat: add AlertaScreen with AlertDialog MD3, spinner and preview

---
+++ shared/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "mx.utng.smarthealthmonitor.shared"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 26
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    // Coroutines para StateFlow
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+}
