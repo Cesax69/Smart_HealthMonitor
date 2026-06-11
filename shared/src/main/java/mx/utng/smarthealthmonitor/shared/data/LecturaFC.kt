ae1762c0dee4ae0b21f76dad6e29441bbb47bad0
Cesax69 <cesar20437@cbtis75.edu.mx>
2026-06-10 19:43:26 -0600

    refactor: extract SmartHealthRepository to shared module

---
+++ shared/src/main/java/mx/utng/smarthealthmonitor/shared/data/LecturaFC.kt
@@ -0,0 +1,9 @@
+﻿package mx.utng.smarthealthmonitor.shared.data
+
+data class LecturaFC(
+    val id: Int = 0,
+    val valorBpm: Int,
+    val timestamp: Long,
+    val hora: String,
+    val esNormal: Boolean
+)
