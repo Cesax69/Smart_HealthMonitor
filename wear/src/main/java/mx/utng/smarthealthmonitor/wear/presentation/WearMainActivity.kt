f525c412b03a61b712975e00a7144e6540fd0e1a
Cesax69 <cesar20437@cbtis75.edu.mx>
2026-06-15 16:03:32 -0600

    fix: explicit watch face feature and manifest indexing fix

--- wear/src/main/java/mx/utng/smarthealthmonitor/wear/presentation/WearMainActivity.kt
+++ wear/src/main/java/mx/utng/smarthealthmonitor/wear/presentation/WearMainActivity.kt
@@ -8,6 +8,7 @@ import mx.utng.smarthealthmonitor.wear.presentation.theme.SmartHealthWearTheme
 class WearMainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        android.widget.Toast.makeText(this, "WatchFace Instalada v3", android.widget.Toast.LENGTH_LONG).show()
         setContent {
             SmartHealthWearTheme {
                 SmartHealthWearNavGraph()
