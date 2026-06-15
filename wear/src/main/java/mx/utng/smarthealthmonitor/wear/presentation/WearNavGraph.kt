12f4e2e74ff213d2e543f5c802bd1f39c058ad00
Cesax69 <cesar20437@cbtis75.edu.mx>
2026-06-15 14:44:52 -0600

    fix: force navigation with shorter routes and navigation toasts

--- wear/src/main/java/mx/utng/smarthealthmonitor/wear/presentation/WearNavGraph.kt
+++ wear/src/main/java/mx/utng/smarthealthmonitor/wear/presentation/WearNavGraph.kt
@@ -1,22 +1,25 @@
 package mx.utng.smarthealthmonitor.wear.presentation
 
+import android.widget.Toast
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.wear.compose.navigation.SwipeDismissableNavHost
 import androidx.wear.compose.navigation.composable
 import androidx.wear.compose.navigation.rememberSwipeDismissableNavController
 
 object WearScreens {
-    const val DASHBOARD = "dashboard"
-    const val ALERTA    = "alerta"
-    const val HISTORIAL = "historial"
+    const val DASHBOARD = "dash"
+    const val ALERTA    = "alert"
+    const val HISTORIAL = "hist"
 }
 
 @Composable
 fun SmartHealthWearNavGraph() {
     val navController = rememberSwipeDismissableNavController()
+    val context = LocalContext.current
 
     SwipeDismissableNavHost(
         navController = navController,
@@ -28,6 +31,7 @@ fun SmartHealthWearNavGraph() {
                     navController.navigate(WearScreens.ALERTA)
                 },
                 onHistoryClick = {
+                    Toast.makeText(context, "Ejecutando navigate...", Toast.LENGTH_SHORT).show()
                     navController.navigate(WearScreens.HISTORIAL)
                 }
             )
