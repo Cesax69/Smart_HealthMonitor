f7ed9248698a6c756ca4e19917d0227cf2d59a39
Cesax69 <cesar20437@cbtis75.edu.mx>
2026-07-10 14:20:24 -0600

    fix: ensures MainFragment is correctly loaded in TV MainActivity

--- tv/src/main/java/mx/utng/smarthealthmonitor/tv/MainActivity.kt
+++ tv/src/main/java/mx/utng/smarthealthmonitor/tv/MainActivity.kt
@@ -6,16 +6,17 @@ import androidx.fragment.app.FragmentActivity
 /**
  * MainActivity para Android TV.
  * Es solo el contenedor: carga MainFragment.
- * TODA la lógica de UI va en el Fragment.
  */
 class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        // Cargar el fragmento solo si es la primera vez que se crea la actividad
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.main_browse_fragment, MainFragment())
-                .commit()
+                .commitNow()
         }
     }
 }
